897ed4e49f54f0917c3827002c23271ad518b031
Prosper Ugbovo [YONGDEV] <85100001+prosperyong@users.noreply.github.com>
2025-01-27 02:24:03 +0100

    chore: fixed tests

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,7 +24,8 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->userName(),
+            'avatar' => fake()->image(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
